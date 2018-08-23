b0be92a1abbbee610b427b62f6e2a822fc4c4419
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-08-23 14:34:49 +1000

    Add pytest tests.

---
+++ helloworld/test_helloworld.py
@@ -0,0 +1,7 @@
+from helloworld import say_hello
+
+def test_helloworld_no_params():
+    assert say_hello() == "Hello, World!"
+
+def test_helloworld_with_param():
+    assert say_hello("Everyone") == "Hello, Everyone!"
