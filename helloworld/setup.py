58aea389e5cc5438fb8cf8f02a8a5dacb6f26a2c
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-08-23 11:32:46 +1000

    (Too) simple project.

---
+++ helloworld/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='helloworld',
+    version='0.0.1',
+    description='Say hello!',
+    py_modules=["helloworld"],
+    package_dir={'': 'src'},
+)
---
+++ helloworld/src/helloworld.py
@@ -0,0 +1,5 @@
+def say_hello(name=None):
+    if name is None:
+        return "Hello, World!"
+    else:
+        return f"Hello, {name}!"
