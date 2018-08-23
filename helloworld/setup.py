a1158d3a3cfc5dfe336cd91e2368f85419653545
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-08-23 15:30:49 +1000

    Trove classifiers, url and author deets.

--- helloworld/setup.py
+++ helloworld/setup.py
@@ -11,4 +11,14 @@ setup(
     package_dir={"": "src"},
     long_description=long_description,
     long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/example/helloworld",
+    author="Mark Smith",
+    author_email="mark.smith@vonage.com",
 )
