c6510dbe53af130a02cad8c81b0ab372cee6dcf4
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-08-23 14:37:42 +1000

    Black reformat

--- helloworld/setup.py
+++ helloworld/setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='helloworld',
-    version='0.0.1',
-    description='Say hello!',
+    name="helloworld",
+    version="0.0.1",
+    description="Say hello!",
     py_modules=["helloworld"],
-    package_dir={'': 'src'},
+    package_dir={"": "src"},
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
--- helloworld/test_helloworld.py
+++ helloworld/test_helloworld.py
@@ -1,7 +1,9 @@
 from helloworld import say_hello
 
+
 def test_helloworld_no_params():
     assert say_hello() == "Hello, World!"
+
 
 def test_helloworld_with_param():
     assert say_hello("Everyone") == "Hello, Everyone!"
