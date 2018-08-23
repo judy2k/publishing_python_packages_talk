91a44518f4d4d9b1c688d1e0dcdee5af249d0ae7
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-08-23 12:14:31 +1000

    Add long description to setup.py

--- helloworld/setup.py
+++ helloworld/setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='helloworld',
     version='0.0.1',
     description='Say hello!',
     py_modules=["helloworld"],
     package_dir={'': 'src'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
